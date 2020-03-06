49f197457f25070584ece7e640c0cb9e23fdb219
sirobettio <siro.bettio@gmail.com>
2020-03-06 09:28:06 +0000

    .

--- viyaci/scripts/sas_admin_profile.sh
+++ viyaci/scripts/sas_admin_profile.sh
@@ -3,7 +3,7 @@
 export SAS_CLI_DEFAULT_CAS_SERVER="cas-shared-default"
 export SAS_CLI_PROFILE="lab13-viya35"
 
-sas-admin profile set-endpoint "http://lab13-viyaweb.lab.local" 
+sas-admin profile set-endpoint "http://lab13-viya35.lab.local" 
 sas-admin profile toggle-color off 
 sas-admin profile set-output fulljson 
 
